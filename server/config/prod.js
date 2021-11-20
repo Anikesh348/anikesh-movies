f7cd6bef3fff0181df1b1bc350ba7660e3bd4a47
Anikesh348 <anikeshthakur85@gmail.com>
2021-11-20 21:14:18 +0530

    secured private info

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB_URI:'mongodb+srv://anikesh:test123@cluster0.oqfkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MONGODB_URI: process.env.MONGODB_URI
 }
